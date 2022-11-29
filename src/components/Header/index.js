b09879b18e481830bf0860798712b7cc7f552b4e
marketamajerova <marketamajerova.6@gmail.com>
2022-11-29 21:00:38 +0100

    Importing components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,22 @@
 export const Header = () => {
-    
+    const element = document.createElement('header');
+
+    element.innerHTML = `<header>
+        <div class="header__content container">
+        <div class="site-logo"></div>
+
+        <div class="navigation">
+            <button class="nav-btn"></button>
+            <nav class="rollout-nav nav-closed">
+            <a href="#home">domů</a>
+            <a href="#menu">menu</a>
+            <a href="#gallery">galerie</a>
+            <a href="#contact">kontakt</a>
+            </nav>
+        </div>
+
+        </div>
+    </header>`
+
+    return element;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './style.css';
+import { Banner } from './components/Banner/index.js';
+import { Contact } from './components/Contact/index.js';
+import { Footer } from './components/Footer/index.js';
+import { Gallery } from './components/Gallery/index.js';
+import { Header } from './components/Header/index.js';
+import { Menu } from './components/Menu/index.js';
+
+
 
 console.log('funguju!');
 
