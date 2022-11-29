1899908f8fd4a7e6558aab0b2fa01c90e32e43be
marketamajerova <marketamajerova.6@gmail.com>
2022-11-29 20:21:30 +0100

    Distribution of CSS styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './style.css';
 
 console.log('funguju!');
+
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(
+  Banner(),
+  Menu(),
+  Gallery(),
+  Contact(),
+);
+
+pageElement.append(
+  Header(),
+  main,
+  Footer(),
+);
+
+document.querySelector('#app').append(pageElement);
