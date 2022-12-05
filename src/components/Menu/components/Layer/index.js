5c4ce1bee85f9105af952e259811ea5f12c2bba9
marketamajerova <marketamajerova.6@gmail.com>
2022-12-05 21:58:31 +0100

    Adding layers via component

---
+++ src/components/Menu/components/Layer/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+
+export const Layer = (props) => {
+    const { color, label } = props;
+
+    const element = document.createElement('div');
+    element.classList.add('layer')
+
+    element.innerHTML = `<div class="layer__color" style="background-color: ${color}"></div>
+                        <div class="layer__label">${label}</div>`;
+    console.log(element);             
+    return element;
+}
+
+/* <div class="layer">
+<div class="layer__color" style="background-color: #613916"></div>
+<div class="layer__label">espresso</div>
+</div> */
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,3 +1,4 @@
+import { Layer } from './components/Layer/index.js';
 import './style.css';
 
 export const Menu = () => {
@@ -21,10 +22,7 @@ export const Menu = () => {
                 </div>
                 <div class="drink__info">
                     <h3>Espresso</h3>
-                    <div class="layer">
-                    <div class="layer__color" style="background-color: #613916"></div>
-                    <div class="layer__label">espresso</div>
-                    </div>
+                    
                 </div>
                 </div>
                 <div class="drink__controls">
@@ -54,36 +52,25 @@ export const Menu = () => {
                 </div>
             </div>
 
-            <div class="drink">
-                <div class="drink__product">
-                <div class="drink__cup">
-                    <img src="https://apps.kodim.cz/daweb/cafelora/assets/cups/lungo.png">
-                </div>
-                <div class="drink__info">
-                    <h3>Lungo</h3>
-                    <div class="layer">
-                    <div class="layer__color" style="background-color: #b0dee1"></div>
-                    <div class="layer__label">voda</div>
-                    </div>
-                    <div class="layer">
-                    <div class="layer__color" style="background-color: #613916"></div>
-                    <div class="layer__label">espresso</div>
-                    </div>
-                </div>
-                </div>
-                <div class="drink__controls">
-                <button class="order-btn">
-                    Objednat
-                </button>
-                </div>
-            </div>
-            </div>
         
             <div class="order-detail">
             <a href="/objednavka">Detail objednávky</a>
             </div>
         </div>
    `
+
+   element.querySelector('.drink__info').append(Layer({
+        color: '#feeeca',
+        label: 'mléčná pěna',
+    }),
+    Layer({
+        color: '#b0dee1"',
+        label: 'voda',
+    }),
+    Layer({
+        color: '#613916',
+        label: 'espresso',
+    }))
 
    return element;
     
